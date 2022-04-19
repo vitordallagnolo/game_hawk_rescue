94280851bd07658df7d3b123d38881a56c3eb864
Dallagnolo <vitor.dallagnolo@gmail.com>
2022-04-19 19:34:15 -0300

    Adicionadas funções de movimento de cenário e iníco das funções de movimentação do jgoador

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,42 @@ function start() {
     $("#inicio").hide();
 
     $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
-    $("#fundoGame").append("<div id='inimigo1'></div>");
+    $("#fundoGame").append("<div id='inimigo1' class='anima2' ></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
-    $("#fundoGame").append("<div id='amigo'></div>");
+    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+    // Principais variáveis do game
+    var jogo = {};
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68,
+    };
+
+    jogo.pressionou = [];
+
+    // Verificar se o jogador pressionou alguma tecla
+    $(document).keydown(function(e) {
+        jogo.pressionou[e.which] = true;
+    });  
+
+    $(document).keyup(function(e) {
+        jogo.pressionou[e.which] = false;
+    });
+    
+
+    // Game Loop
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+        moveFundo();
+        moveJogador();
+    };
+
+    // Função que move o cenário
+    function moveFundo() {
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position", esquerda - 1);
+    };
 }
+
